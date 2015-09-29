2af776e1905d1a0d6531a36d557b1cead5573e52
Lorna Marlow <bikergirl1515@hotmail.com>
2015-09-29 11:49:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marketharborough::Application.config.secret_key_base = 'afb306033e2a8ee8951c6ecf745290663c0ff8f8eddbf706d2375b55ad47ef5f741847bb865346aaf58af3e3e099de35d32633effc684a37cb2ce8cb33f91d1f'
