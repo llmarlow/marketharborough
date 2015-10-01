0d2c0a93fe37579f6c0974ddc2af50a100a2f5b1
Lorna Marlow <bikergirl1515@hotmail.com>
2015-10-01 12:56:41 +0000

    Added validations to photos and styling of places show page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ActiveRecord::Base
 	belongs_to :place
 	mount_uploader :picture, PictureUploader
+
+	validates :picture, :caption, presence: true
+	validates :caption, length: { minimum: 5 }
 end
